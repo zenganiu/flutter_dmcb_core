76d4a4de79e2f185d6eaa00b9e735a42c244764b
xuhuimin <1126981418@qq.com>
2023-03-01 09:53:33 +0800

    扩展方法增加dm前缀

--- lib/extension/ex_string.dart
+++ lib/extension/ex_string.dart
@@ -9,7 +9,7 @@ extension DcmbExString on String {
   /// 字符串空处理 (''或null)
   ///
   /// [defaultValue] 默认值,默认: 无
-  String sensorNoEmpty({String defaultValue = '无'}) {
+  String dmSensorNoEmpty({String defaultValue = '无'}) {
     var str = this;
     if (str.isEmpty || str == 'null') {
       str = '无';
@@ -17,10 +17,15 @@ extension DcmbExString on String {
     return str;
   }
 
+  @Deprecated('使用`dmSensorNoEmpty`代替')
+  String sensorNoEmpty({String defaultValue = '无'}) {
+    return dmSensorNoEmpty(defaultValue: defaultValue);
+  }
+
   /// hex转换成Color对象
   ///
   /// [defaultColor] 默认颜色，默认为黑色
-  Color toColor({Color defaultColor = Colors.black}) {
+  Color dmToColor({Color defaultColor = Colors.black}) {
     final sb = StringBuffer();
     if (length == 6 || length == 7) sb.write('ff');
     sb.write(replaceAll('#', ''));
@@ -32,15 +37,25 @@ extension DcmbExString on String {
     }
   }
 
+  @Deprecated('使用`dmToColor`代替')
+  Color toColor({Color defaultColor = Colors.black}) {
+    return dmToColor(defaultColor: defaultColor);
+  }
+
   /// md5加密
-  String toMD5() {
+  String dmToMD5() {
     var content = const convert.Utf8Encoder().convert(this);
     var digest = md5.convert(content);
     return digest.toString();
   }
 
+  @Deprecated('使用`dmToMD5`代替')
+  String toMD5() {
+    return dmToMD5();
+  }
+
   /// 转为int
-  int? toInt() {
+  int? dmToInt() {
     try {
       return int.tryParse(this);
     } catch (e) {
@@ -48,9 +63,19 @@ extension DcmbExString on String {
     }
   }
 
+  @Deprecated('使用`dmToInt`代替')
+  int? toInt() {
+    return dmToInt();
+  }
+
   /// 是否是手机号码
+  bool dmIsPhone() {
+    return dmRegExpMatch(r'^((13[0-9])|(14[0-9])|(15[0-9])|(16[0-9])|(17[0-9])|(18[0-9])|(19[0-9]))\d{8}$');
+  }
+
+  @Deprecated('使用`dmIsPhone`代替')
   bool isPhone() {
-    return regExpMatch(r'^((13[0-9])|(14[0-9])|(15[0-9])|(16[0-9])|(17[0-9])|(18[0-9])|(19[0-9]))\d{8}$');
+    return dmIsPhone();
   }
 
   /// 字符串替换
@@ -58,13 +83,18 @@ extension DcmbExString on String {
   /// [start] 起始位置
   /// [end] 结束位置
   /// [replacement] 替换内容
-  String replaceString({int start = 3, int end = 7, String replacement = '****'}) {
+  String dmReplaceString({int start = 3, int end = 7, String replacement = '****'}) {
     if (start < 0 || length < end || start > end) return this;
     return replaceRange(start, end, replacement);
   }
 
+  @Deprecated('使用`dmReplaceString`代替')
+  String replaceString({int start = 3, int end = 7, String replacement = '****'}) {
+    return dmReplaceString(start: start, end: end, replacement: replacement);
+  }
+
   /// 反转字符串
-  String reverse() {
+  String dmReverse() {
     if (isEmpty) return '';
     StringBuffer sb = StringBuffer();
     for (int i = length - 1; i >= 0; i--) {
@@ -73,14 +103,29 @@ extension DcmbExString on String {
     return sb.toString();
   }
 
+  @Deprecated('使用`dmReverse`代替')
+  String reverse() {
+    return dmReverse();
+  }
+
   /// 正则判断
-  bool regExpMatch(String expStr) {
+  bool dmRegExpMatch(String expStr) {
     RegExp exp = RegExp(expStr);
     return exp.hasMatch(this);
   }
 
+  @Deprecated('使用`dmRegExpMatch`代替')
+  bool regExpMatch(String expStr) {
+    return dmRegExpMatch(expStr);
+  }
+
   /// 移除所有空格
-  String trimAll() {
+  String dmTrimAll() {
     return replaceAll(RegExp(r"\s+\b|\b\s"), "");
+  }
+
+  @Deprecated('使用`dmTrimAll`代替')
+  String trimAll() {
+    return dmTrimAll();
   }
 }
